0474f15f99d2cf087bb76009d7b5c3d3c9d7b73b
Walendemar <vladimir@MacBook-Pro-Vladimir.local>
2023-09-22 19:41:15 +0300

    #4 Added TS and removed Babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,35 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["airbnb", "prettier"],
-  plugins: ["prettier", "react", "react-hooks"],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "prettier",
+    "plugin:react/jsx-runtime",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  plugins: ["prettier", "react", "react-hooks", "@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+    },
+  ],
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".css", ".js", ".ts", ".tsx"],
+      },
+    },
   },
   rules: {
     "arrow-body-style": "off",
--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -3,72 +3,70 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const isProduction =
-	process.argv.some((arg) => arg === "-p" || arg === "--production") ||
-	process.env.NODE_ENV === "production";
+  process.argv.some((arg) => arg === "-p" || arg === "--production") ||
+  process.env.NODE_ENV === "production";
 
 const ROOT_DIR = path.resolve(__dirname, "..");
 
 const PATHS = {
-	app: path.resolve(ROOT_DIR, "src"),
-	build: path.resolve(ROOT_DIR, "dist"),
+  app: path.resolve(ROOT_DIR, "src"),
+  build: path.resolve(ROOT_DIR, "dist"),
 };
 
-console.log(path.resolve(ROOT_DIR, "template/template.html"));
-
 const config = {
-	mode: isProduction ? "production" : "development",
-	devtool: isProduction ? "hidden-source-map" : "eval-cheap-module-source-map",
-	entry: {
-		app: PATHS.app,
-	},
-	output: {
-		path: PATHS.build,
-		filename: "app.js",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /(node_modules)/,
-				loader: require.resolve("babel-loader"),
-				options: {
-					compact: false,
-					cacheCompression: false,
-					cacheDirectory: true,
-				},
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: "Knowledge",
-			template: path.resolve(ROOT_DIR, "./template/template.html"),
-			filename: "index.html",
-		}),
-	],
-	resolve: {
-		extensions: [".js", ".jsx"],
-		modules: ["node_modules"],
-		alias: {
-			"@components": path.resolve(PATHS.app, "components"),
-			"@utils": path.resolve(PATHS.app, "utils"),
-		},
-	},
-	devServer: {
-		static: {
-			directory: PATHS.dist,
-		},
-		compress: true,
-		port: "8070",
-		historyApiFallback: {
-			disableDotRule: true,
-		},
-		hot: true,
-	},
+  mode: isProduction ? "production" : "development",
+  devtool: isProduction ? "hidden-source-map" : "eval-cheap-module-source-map",
+  entry: {
+    app: PATHS.app,
+  },
+  output: {
+    path: PATHS.build,
+    filename: "app.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Knowledge",
+      template: path.resolve(ROOT_DIR, "./template/template.html"),
+      filename: "index.html",
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    modules: ["node_modules"],
+    alias: {
+      "@components": path.resolve(PATHS.app, "components"),
+      "@utils": path.resolve(PATHS.app, "utils"),
+    },
+  },
+  devServer: {
+    static: {
+      directory: PATHS.dist,
+    },
+    compress: true,
+    port: "8070",
+    historyApiFallback: {
+      disableDotRule: true,
+    },
+    hot: true,
+  },
 };
 
 if (isProduction) {
-	config.plugins.push(new CleanWebpackPlugin());
+  config.plugins.push(new CleanWebpackPlugin());
 }
 
 module.exports = config;
